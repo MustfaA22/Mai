0356db08126b48918073f448ce1d2e8fad153de9
Ali Jabar <ali93456@gmail.com>
2023-07-31 20:00:17 +0300

    add clickable logo and shadows around the active navlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,31 +6,38 @@ import { NavContext } from "../context/NavContext";
 const navLinks = ["home", "program-aspects", "goals", "who-are-we", "contact-us"];
 const navLinksContent = [" الصفحة الرئيسية", "جوانب البرنامج", "الاهداف", "من نحن", "تواصل معنا"];
 
-function renderNavLink (content, i, activeLinkId) {
+function Navbar() {
+
+  const { activeLinkId } = useContext(NavContext);
   
-  const handleClickNav = () => {
-    document.getElementById(content).scrollIntoView({behavior: "smooth", block: "center" });
+  
+  const handlerClickLogo = () => {
+    document.getElementById("home").scrollIntoView({behavior: "smooth", block: "center" });
   }
   
-  return (
-    <ul key={content}>
-      <li>
-        <button 
-          onClick={handleClickNav} 
-          className={activeLinkId === content ? classes.active : ""}
-          > {navLinksContent[i]} </button>
-      </li>
-    </ul>
-  )
-} 
+  function renderNavLink (content, i, activeLinkId) {  
+    const handleClickNav = () => {
+      document.getElementById(content).scrollIntoView({behavior: "smooth", block: "center" });
+    }
+    
+    return (
+      <ul key={content}>
+        <li>
+          <button 
+            onClick={handleClickNav} 
+            className={activeLinkId === content ? classes.active : ""}
+            > {navLinksContent[i]} </button>
+        </li>
+      </ul>
+    )
+  }
 
-function Navbar() {
-  const { activeLinkId } = useContext(NavContext);
+
 
   return (
       <nav className={classes.navbar}>
         <div className={classes.logo}>
-          <img src={logo} style={{width: "70px", objectFit: "contain", objectPosition: "center"}} alt="" />
+          <img src={logo} style={{width: "70px", objectFit: "contain", objectPosition: "center"}} alt="logo" onClick={handlerClickLogo}/>
         </div>
         <div className={classes.links}>
               {navLinks.map((nav, i) => 
