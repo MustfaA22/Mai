62aac7a0bd426be4b252a285c1a0ad68ef1e5397
Ali Jabar <ali93456@gmail.com>
2023-08-01 15:29:12 +0300

    add footer I used react icons lib to add social media icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,43 @@
+import { BiLogoFacebookCircle } from "react-icons/bi";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { AiFillInstagram } from "react-icons/ai"
+import maiWhiteLogo from "../assets/images/logo-white.svg";
+import iotKidsLogo from "../assets/images/iot-kids-logo.svg";
+
+import classes from "../styles/Footer.module.css";
+
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className={classes.footer}>
+      <div className={classes.logos}>
+        <img 
+          src={maiWhiteLogo} 
+          alt="mai logo" 
+          style={{width: "160px", height: "auto", margin: "0 15px", display: "block", objectFit: "contain"}}
+        />
+        <a href="https://iotkidsiq.com/" target="_blank" rel="noreferrer">
+          <img 
+            src={iotKidsLogo} 
+            alt="iot kids logo" 
+            style={{width: "160px", height: "auto", margin: "0 15px", display: "block", objectFit: "contain"}}
+          />
+        </a>
+
+        <div className="social" style={{display: "flex", justifyContent: "center", alignItems: "center", gap: "1em"}}>
+          <a href="https://www.facebook.com/IoTKIDS/" target="_blank" rel="noreferrer">
+            <BiLogoFacebookCircle style={{width: "56px", height: "auto", color: "#fff"}} />
+          </a>
+          <a href="#" target="_blank" rel="noreferrer">
+            <IoLogoWhatsapp style={{width: "56px", height: "auto", color: "#fff"}} />
+          </a>
+          <a href="https://www.instagram.com/iotkids/" target="_blank" rel="noreferrer">
+            <AiFillInstagram style={{width: "56px", height: "auto", color: "#fff"}} />
+          </a>
+        </div>
+      </div>
+
+      <p className={classes.rightsReservedText}> all right reserved 2023 </p>
+    </footer>
   )
 }
 
