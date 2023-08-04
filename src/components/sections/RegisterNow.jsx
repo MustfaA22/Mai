826bc093f6c46cda28ae01b2e1231284fb228915
Ali Jabar <ali93456@gmail.com>
2023-08-05 02:15:44 +0300

    create registerNow section

--- src/components/sections/RegisterNow.jsx
+++ src/components/sections/RegisterNow.jsx
@@ -1,13 +1,22 @@
-import { useNav } from "../../hooks/useNav"
+import { useNav } from "../../hooks/useNav";
+import classes from "../../styles/sectionStyles/RegisterNow.module.css";
 
 function RegisterNow() {
   const registerNowRef = useNav();
 
   return (
     <section ref={registerNowRef} id="register-now">
-      RegisterNow
+      <div className={classes.contentWrapper}>
+        <h1> سَجلي اﻵن! </h1>
+        <p> وأبدئي رحلتكِ في مجال التكنولوجيا </p>
+        <button className={classes.registerNowBtn}>
+          <span className={classes.shadow}></span>
+          <span className={classes.edge}></span>
+          <span className={`${classes.front} ${classes.text}`}>  سَجلي اﻵن </span>
+        </button>
+      </div>
     </section>
   )
 }
 
-export default RegisterNow
+export default RegisterNow;
