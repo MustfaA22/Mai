51694678191fbd6592eba455f9e833037140b777
Ali Jabar <ali93456@gmail.com>
2023-08-07 02:50:24 +0300

    create contest section + make it responsive

--- src/components/sections/Contest.jsx
+++ src/components/sections/Contest.jsx
@@ -1,11 +1,26 @@
 import { useNav } from "../../hooks/useNav";
+import competition from "../../assets/images/competition/competition.png";
+import classes from "../../styles/sectionStyles/Contest.module.css";
+import Button from "../../UI/Button";
+
 
 function Contest() {
   const contestRef = useNav();
 
   return (
     <section ref={contestRef} id="contest">
-      Contest
+      <div className={classes.center}>
+        <div className={classes.contentWrapper}>
+          <div className={classes.textAndButton}>
+            <h1> المنافسة </h1>
+            <p>
+              هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى. هذا النص العربي هو مثال.
+            </p>
+            <Button buttonText={"سَجلي اﻵن"} />
+          </div>
+            <img src={competition} alt="competition" />
+        </div>
+      </div>
     </section>
   )
 }
