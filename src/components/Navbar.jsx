7840c70ccd7cac97f8e65d4f92edb92e7710f84c
Ali Jabar <ali93456@gmail.com>
2023-08-08 21:37:23 +0300

    make the navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
+import { useContext, useState } from "react";
+import { NavContext } from "../context/NavContext";
+
 import classes from "../styles/Navbar.module.css";
 import logo from "../assets/images/logo.svg";
-import { useContext } from "react";
-import { NavContext } from "../context/NavContext";
+import menu from "../assets/images/burger-bar.svg";
+import close from "../assets/images/close.svg";
 
 const navLinks = ["sponsors", "contest", "register-now", "program-content", "home"];
 const navLinksContent = ["الرعاة", "المسابقة", "سجل الان", "محتوى البرنامج", "الصفحة الرئيسية"];
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
 
   const { activeLinkId } = useContext(NavContext);
   
@@ -36,17 +40,25 @@ function Navbar() {
       <nav className={classes.navbar}>
         <div className={classes.logo}>
           <img 
-            src={logo} 
-            style={{width: "60px", objectFit: "contain", objectPosition: "center"}} 
+            src={logo}
             alt="logo"
             onClick={handlerClickLogo}
           />
         </div>
-        <div className={classes.links}>
-              {navLinks.map((nav, i) => 
+        <button 
+          className={classes.mobileNavToggle} 
+          onClick={() => setIsOpen(true)}>
+            <img src={menu} alt="menu" />
+        </button>
+        <div className={classes.links} data-visible={isOpen ? "true" : "false"}>
+          <button onClick={() => setIsOpen(false)}>
+            <img src={close} className={classes.close} />
+          </button>
+              {navLinks.map((nav, i) =>
                 renderNavLink(nav, i, activeLinkId)
               )}
         </div>
+              
       </nav>
   )
 }
