3390899a9603cf38e2aba594310cce187834b3d5
MustfaA22 <mustfaas20000@gmail.com>
2023-08-17 03:01:07 +0300

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,81 +5,93 @@ import classes from "../styles/Navbar.module.css";
 import logo from "../assets/images/logo-white.svg";
 import menu from "../assets/images/burger-bar.svg";
 import close from "../assets/images/close.svg";
-import enimg from "../assets/images/en.png"
-import arimg from "../assets/images/ar.png" 
- 
-const navLinks = ["sponsors", "contest", "register-now", "program-content", "home"];
-const navLinksContentAR = ["الرعاة", "المسابقة", "سجل الان", "محتوى البرنامج", "الصفحة الرئيسية"];
-const navLinksContentEn = ["Sponsors", "Competition", "Registeration", "Program Content", "Home"];
-const lang =localStorage.getItem("lang")|| "ar"
+import enimg from "../assets/images/en.png";
+import arimg from "../assets/images/ar.png";
+
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false); 
+  const [isOpen, setIsOpen] = useState(false);
+  const lang = localStorage.getItem("lang") || "ar";
+  const { activeLinkId } = useContext(NavContext);
+
   const langChange = (option) => {
     localStorage.setItem("lang", option);
     window.location.reload();
-    window.scrollTo(0,0)
-  }
-  const { activeLinkId } = useContext(NavContext);
-  
+    window.scrollTo(0, 0);
+  };
+
   const handlerClickLogo = () => {
     document.getElementById("home").scrollIntoView({ behavior: "smooth", block: "start" });
-  }
-  
-  // function to render nav links
-  function renderNavLink (content, i, activeLinkId) {
-    const handleClickNav = () => {
-      document.getElementById(content).scrollIntoView({ behavior: "smooth", block: "start" });
-    }
-
-    return (
-      <ul key={content}>
-        
-        <li>
-          <button 
-            onClick={handleClickNav} 
-            className={activeLinkId === content ? classes.active : ""}
-            > { lang=="ar"? navLinksContentAR[i]:navLinksContentEn[i]} </button>
-        </li>
-        
-      </ul>
-    )
-  }
+  };
 
   return (
-      <nav className={classes.navbar}>
-
-          {localStorage.getItem("lang")=="ar"?
-       <button className="d-flex" onClick={() => langChange("en")} >
-       <img className={classes.flags} src={enimg} alt="" />
-   </button>
-   
-         :  <button className="d-flex" onClick={() => langChange("ar")} >
-         <img className={classes.flags} src={arimg} alt="" />
-     </button>}
-        <button 
-          className={classes.mobileNavToggle} 
-          onClick={() => setIsOpen(true)}>
-            <img src={menu} alt="menu" />
+    <nav className={classes.navbar}>
+      {lang === "ar" ? (
+        <button className="d-flex" onClick={() => langChange("en")}>
+          <img className={classes.flags} src={enimg} alt="" />
         </button>
-        <div className={classes.links} data-visible={isOpen ? "true" : "false"}>
-          <button onClick={() => setIsOpen(false)}>
-            <img src={close} className={classes.close} />
-          </button>
-              
-              {navLinks.map((nav, i) =>
-                renderNavLink(nav, i, activeLinkId)
-              )}
-        </div>
-        
-        <div className={classes.logo}>
-               <img 
-                 src={logo}
-                 alt="logo"
-                 onClick={handlerClickLogo}
-               />
-             </div>
-      </nav>
-  )
+      ) : (
+        <button className="d-flex" onClick={() => langChange("ar")}>
+          <img className={classes.flags} src={arimg} alt="" />
+        </button>
+      )}
+
+      <button className={classes.mobileNavToggle} onClick={() => setIsOpen(true)}>
+        <img src={menu} alt="menu" />
+      </button>
+      <div className={classes.links} data-visible={isOpen ? "true" : "false"}>
+        <button onClick={() => setIsOpen(false)}>
+          <img src={close} className={classes.close} />
+        </button>
+
+        <ul className={classes.navLinksNav}>
+          <li>
+            <button
+              onClick={() => document.getElementById("sponsors").scrollIntoView({ behavior: "smooth", block: "start" })}
+              className={activeLinkId === "sponsors" ? classes.active : ""}
+            >
+              {lang === "ar" ? "الرعاة" : "Sponsors"}
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => document.getElementById("contest").scrollIntoView({ behavior: "smooth", block: "start" })}
+              className={activeLinkId === "contest" ? classes.active : ""}
+            >
+              {lang === "ar" ? "المسابقة" : "Competition"}
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => document.getElementById("register-now").scrollIntoView({ behavior: "smooth", block: "start" })}
+              className={activeLinkId === "register-now" ? classes.active : ""}
+            >
+              {lang === "ar" ? "سجل الان" : "Registeration"}
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => document.getElementById("program-content").scrollIntoView({ behavior: "smooth", block: "start" })}
+              className={activeLinkId === "program-content" ? classes.active : ""}
+            >
+              {lang === "ar" ? "محتوى البرنامج" : "Program Content"}
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => document.getElementById("home").scrollIntoView({ behavior: "smooth", block: "start" })}
+              className={activeLinkId === "home" ? classes.active : ""}
+            >
+              {lang === "ar" ? "الصفحة الرئيسية" : "Home"}
+            </button>
+          </li>
+        </ul>
+      </div>
+
+      <div className={classes.logo}>
+        <img src={logo} alt="logo" onClick={handlerClickLogo} />
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
